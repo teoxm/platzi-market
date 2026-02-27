682fe41fdbbdc4ce79412c7dbf8e1248101e4461
teofilo.mendez <teofilo.mendez@ejercito.mil.co>
2026-02-27 14:52:41 -0500

    Se crean los mappers para purchase y pursacheItems

---
+++ src/main/java/com/platzi/market/domain/PurchaseItem.java
@@ -0,0 +1,40 @@
+package com.platzi.market.domain;
+
+public class PurchaseItem {
+    private int productoId;
+    private int quantity;
+    private double total;
+    private boolean active;
+
+    public int getProductoId() {
+        return productoId;
+    }
+
+    public void setProductoId(int productoId) {
+        this.productoId = productoId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
